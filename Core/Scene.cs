a9b68cc49b66867ba9517bc2331ef31f496fc380
syskov <s.a.syskov@yandex.ru>
2026-05-20 03:38:54 +0500

    add Scene base class with entity management

---
+++ Core/Scene.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TryToEscape.Core;
+
+public class Scene
+{
+    readonly List<Entity> _entities = new();
+
+    public void AddEntity(Entity entity)
+    {
+        _entities.Add(entity);
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+        foreach (var e in _entities)
+            e.Update(gameTime);
+    }
+
+    public virtual void Draw(SpriteBatch spriteBatch)
+    {
+        foreach (var e in _entities)
+            e.Draw(spriteBatch);
+    }
+}

80f6325b45825c46ea5c52691cdabfb7b5c21f8c
syskov <s.a.syskov@yandex.ru>
2026-05-27 17:36:30 +0500

    add MazeGenerator skeleton with BSP methods

---
+++ World/BSPNode.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+
+namespace TryToEscape.World;
+
+public class BSPNode
+{
+    public Rectangle Area;
+    public Rectangle? Room;
+    public BSPNode? Left;
+    public BSPNode? Right;
+
+    public BSPNode(Rectangle area)
+    {
+        Area = area;
+    }
+}

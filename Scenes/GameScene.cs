84f67e19e735211359807bd60317d348587707ef
syskov <s.a.syskov@yandex.ru>
2026-05-28 14:47:49 +0500

    spawn player at generated start position using BSP leftmost room

--- Scenes/GameScene.cs
+++ Scenes/GameScene.cs
@@ -13,24 +13,28 @@ public class GameScene : Scene
     private MazeRenderer _mazeRenderer;
     private Camera _camera;
     private Entity _spriteEntity;
+    private const int TileSize = 16;
     
 
     public GameScene(ContentManager contentManager, GraphicsDevice graphicsDevice)
     {
         _camera = new Camera(graphicsDevice.Viewport.Width, graphicsDevice.Viewport.Height);
         var sprite = contentManager.Load<Texture2D>("assets/sprite");
-        var maze = new MazeGenerator().Generate(50, 30, 8, 4, 1);
+        var generator = new MazeGenerator();
+        var maze = generator.Generate(50, 30, 8, 4, 1);
+        var start = generator.GetStartPosition();
+        var end = generator.GetEndPosition();
         
         _spriteEntity = new Entity();
         _spriteEntity.AddComponent(new SpriteComponent(sprite));
         _spriteEntity.AddComponent(new InputComponent(100));
-        _spriteEntity.Position = new Vector2(5 * 16, 5 * 16);
-        _spriteEntity.AddComponent(new ColliderComponent(maze, 16, 16));
+        _spriteEntity.Position = new Vector2(start.X * TileSize, start.Y * TileSize);
+        _spriteEntity.AddComponent(new ColliderComponent(maze, TileSize, TileSize));
         AddEntity(_spriteEntity);
 
         var floorTexture = contentManager.Load<Texture2D>("assets/tiles/floor");
         var wallTexture  = contentManager.Load<Texture2D>("assets/tiles/wall");
-        _mazeRenderer = new MazeRenderer(maze, floorTexture, wallTexture, 16);
+        _mazeRenderer = new MazeRenderer(maze, floorTexture, wallTexture, TileSize);
     }
 
     public override Matrix GetCameraMatrix()
